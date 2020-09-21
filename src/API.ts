a479ba3e153deb8f40ca16b212ec4aae276ec587
RaoAkif <61361037+RaoAkif@users.noreply.github.com>
2020-09-21 14:48:48 +0500

    Completed

--- src/API.ts
+++ src/API.ts
@@ -1,4 +1,13 @@
-import React from 'react'
+import { shuffleArray } from './utils';
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
 
 export enum Difficulty {
   EASY = "easy",
@@ -6,8 +15,13 @@ export enum Difficulty {
   HARD = "hard",
 }
 
-export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
-  const endpoint = `http://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
-  const data = await (await fetch (endpoint)).json();
-  console.log(data)
-}
+export type QuestionsState = Question & { answers: string[] };
+
+export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty): Promise<QuestionsState[]> => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
+  }))
+};
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import { RefactorActionInfo } from 'typescript'
+import React from 'react';
+// Types
+import { AnswerObject } from '../App';
+// Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles';
 
 type Props = {
   question: string;
   answers: string[];
-  callback: any;
-  userAnswer: string;
+  callback: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  userAnswer: AnswerObject | undefined;
   questionNr: number;
-  totalQuestions:number
-}
+  totalQuestions: number;
+};
 
-export const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNr, totalQuestions }) => {
-  return (
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswer,
+  questionNr,
+  totalQuestions,
+}) => (
+  <Wrapper>
+    <p className='number'>
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question }} />
     <div>
-      <p className="number">Question: {questionNr} / {totalQuestions} </p>
-      <p dangerouslySetInnerHTML={{__html: question}} />
-    <div>
-
-    </div>
-      {answers.map(answer => (
-        <div>
+      {answers.map((answer) => (
+        <ButtonWrapper
+          key={answer}
+          correct={userAnswer?.correctAnswer === answer}
+          userClicked={userAnswer?.answer === answer}
+        >
           <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
-            <span dangerouslySetInnerHTML={{__html:answer}} />
+            <span dangerouslySetInnerHTML={{ __html: answer }} />
           </button>
-        </div>
+        </ButtonWrapper>
       ))}
     </div>
-  )
-}
+  </Wrapper>
+);
+
+export default QuestionCard;
